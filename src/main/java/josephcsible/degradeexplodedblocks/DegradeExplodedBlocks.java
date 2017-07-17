f8162f81c4aa5dcbe5a977a80ea24089553f4c80
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-07-16 23:33:58 -0400

    Give the mod a name

--- src/main/java/josephcsible/degradeexplodedblocks/DegradeExplodedBlocks.java
+++ src/main/java/josephcsible/degradeexplodedblocks/DegradeExplodedBlocks.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package josephcsible.degradeexplodedblocks;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = DegradeExplodedBlocks.MODID, version = DegradeExplodedBlocks.VERSION)
+public class DegradeExplodedBlocks
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "degradeexplodedblocks";
+    public static final String VERSION = "1.0.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
