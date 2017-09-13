13abe3664e99e269d0263cb6207be7453081743a
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-09-13 16:25:49 -0400

    Bump version

--- src/main/java/josephcsible/degradeexplodedblocks/DegradeExplodedBlocks.java
+++ src/main/java/josephcsible/degradeexplodedblocks/DegradeExplodedBlocks.java
@@ -51,7 +51,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class DegradeExplodedBlocks {
 	// XXX duplication with mcmod.info and build.gradle
 	public static final String MODID = "degradeexplodedblocks";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	protected static final String[] DEFAULT_REPLACEMENTS = {
 		"minecraft:cobblestone * minecraft:gravel",
